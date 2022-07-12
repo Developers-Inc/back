2754c3fe13d04855b4e3e4a4dd014a525ba4fdea
Marcelo Augusto Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2022-07-12 20:53:59 -0300

    Paulo/1 (#2)
    * [1] - Initialize spring project
    
    * [1] - Build endpoint to test
    
    * [1] - Updated hello world
    
    Co-authored-by: Paulo Teixeira <pteixeira.dev@gmail.com>

---
+++ src/main/java/com/developersinc/back/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.developersinc.back.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "Hello world!";
+    }
+}
