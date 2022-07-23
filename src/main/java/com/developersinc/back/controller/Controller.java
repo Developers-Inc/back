e39b06ea06d4c4652c5165b018d4d01fff0c2c83
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2022-07-22 23:59:43 -0300

    Added cross origins

--- src/main/java/com/developersinc/back/controller/Controller.java
+++ src/main/java/com/developersinc/back/controller/Controller.java
@@ -1,13 +1,15 @@
 package com.developersinc.back.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/api/test")
     public String test() {
-        return "Hello world!";
+        return "Hello world!!!";
     }
 }
