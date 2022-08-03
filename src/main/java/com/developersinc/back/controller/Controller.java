e33754ca9759be372ff8053a86088b431997d8f2
Paulo Teixeira <pteixeira.dev@gmail.com>
2022-08-02 21:30:45 -0300

    [9] - Improve project to avoid CORS origim error

--- src/main/java/com/developersinc/back/controller/Controller.java
+++ src/main/java/com/developersinc/back/controller/Controller.java
@@ -1,13 +1,15 @@
 package com.developersinc.back.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @CrossOrigin("*")
     @GetMapping("/api/test")
     public String test() {
-        return "Hello world!";
+        return "hello world from back-end!";
     }
 }
