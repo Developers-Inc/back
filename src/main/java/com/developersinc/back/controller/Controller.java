78a57a6b094653e508b9e27ce18e243d6f7184db
Paulo Teixeira <45825191+pauloteixeira-dev@users.noreply.github.com>
2022-08-02 21:37:26 -0300

    Paulo/9 (#10)
    * [9] - Improve project to avoid CORS origim error
    
    * Improve code to have capital letter
    
    Co-authored-by: Marcelo Augusto Cordeiro <marcelo.augusto.cordeiro@gmail.com>
    
    Co-authored-by: Marcelo Augusto Cordeiro <marcelo.augusto.cordeiro@gmail.com>

--- src/main/java/com/developersinc/back/controller/Controller.java
+++ src/main/java/com/developersinc/back/controller/Controller.java
@@ -1,13 +1,16 @@
 package com.developersinc.back.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @CrossOrigin("*")
     @GetMapping("/api/test")
     public String test() {
-        return "Hello world!";
+        return "Hello world from back-end!";
+
     }
 }
