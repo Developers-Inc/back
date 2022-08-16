894c57dfe198c7975e4cebf988e63bac59e1d858
Elekmilton Mangino <elek08@gmail.com>
2022-08-16 17:56:35 -0300

    Improve project to avoid CORS origin error

--- src/main/java/com/developersinc/back/Controller.java
+++ src/main/java/com/developersinc/back/Controller.java
@@ -1,12 +1,14 @@
 package com.developersinc.back;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 public class Controller {
     @GetMapping("api/test")
     public String test(){
-        return "Hello World";
+        return "Hello World from backend";
     }
 }
