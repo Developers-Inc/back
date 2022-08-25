e8d84bcff8e84214e1979c77deb7ba97b23ab09b
Fernanda Soares <fernanda.soares.silva@gmail.com>
2022-08-24 21:31:24 -0300

    Updated endpoint to avoid CORS error

--- src/main/java/com/developersinc/back/Controller.java
+++ src/main/java/com/developersinc/back/Controller.java
@@ -1,8 +1,10 @@
 package com.developersinc.back;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class Controller {
     @GetMapping("/api/test")
